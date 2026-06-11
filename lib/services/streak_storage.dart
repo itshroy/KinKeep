5927fbd1b76bdaf6a69fcf30615ce3e78fadd0a4
itshroy <naimish253@gmail.com>
2026-06-11 21:43:34 +0530

    implemented streak increment feature

--- lib/services/streak_storage.dart
+++ lib/services/streak_storage.dart
@@ -2,6 +2,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class StreakStorage {
   static const String streakKey = "streak";
+  static const String lastCompletedDateKey = "lastCompletedDate";
 
   static Future<void> saveStreak(int streak) async {
     final prefs = await SharedPreferences.getInstance();
@@ -13,5 +14,17 @@ class StreakStorage {
     final prefs = await SharedPreferences.getInstance();
 
     return prefs.getInt(streakKey) ?? 0;
+  }
+
+  static Future<void> saveLastCompletedDate(String date) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setString(lastCompletedDateKey, date);
+  }
+
+  static Future<String?> getLastCompletedDate() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.getString(lastCompletedDateKey);
   }
 }
